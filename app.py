a20b11311de739cf834d75d63cc3a122534cb04c
redl1k <andrian14756@gmail.com>
2022-10-18 01:49:38 +0300

    1
    1

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "123"
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run()
