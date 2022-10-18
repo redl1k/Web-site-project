b0268b2cbeb96d66d50aad85c495103005c9dfc4
redl1k <andrian14756@gmail.com>
2022-10-18 20:48:28 +0300

    update

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect 
+from werkzeug.exceptions import abort
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
 
-# @app.route("/")
-# def hello():
-#     return "123"
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-@app.route("/")
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    return render_template('create.html')
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
-
-
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
 
 if __name__ == "__main__":
     app.run()
